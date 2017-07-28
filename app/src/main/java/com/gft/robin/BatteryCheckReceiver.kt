041912a708e0851bed3a54fb351d0d3630cc45ec
tamilazhagan.m <tamilazhagan.m@gofrugal.com>
2017-07-28 19:52:54 +0530

    battery status check solved

--- app/src/main/java/com/gft/robin/BatteryCheckReceiver.kt
+++ app/src/main/java/com/gft/robin/BatteryCheckReceiver.kt
@@ -4,33 +4,95 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.media.RingtoneManager
 import android.os.BatteryManager
-import android.widget.Toast
+import android.util.Log
 
 
 class BatteryCheckReceiver: BroadcastReceiver() {
-    val main = MainActivity()
     override fun onReceive(context: Context?, intent: Intent?) {
-        context!!.applicationContext.registerReceiver(main.mBatInfoReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        context!!.applicationContext.registerReceiver(mBatInfoReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
     }
-
     private val mBatInfoReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
+            val alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)!!
+            val mMediaPlayer = MediaPlayer()
             val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
+            val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
             if (level == 100) {
                 try {
-                    Toast.makeText(context, "HEY", Toast.LENGTH_SHORT).show()
-                    val notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
-                    val r = RingtoneManager.getRingtone(context.applicationContext, notification)
-                    r.play()
-                    //val pm = context.packageManager
-                    //val launchIntent = pm.getLaunchIntentForPackage("com.gft.robin")
-                    // context.startActivity(launchIntent)
+                    mMediaPlayer.setDataSource(context, alert)
+                    if (audioManager.getStreamVolume(AudioManager.STREAM_RING) != 0) {
+                        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_RING)
+                        mMediaPlayer.isLooping = true
+                        mMediaPlayer.prepare()
+                        mMediaPlayer.start()
+                        val pm = context.packageManager
+                        val launchIntent = pm.getLaunchIntentForPackage("com.gft.robin")
+                        context.startActivity(launchIntent)
+                        call(context)
+                    }
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
             }
         }
     }
+
+    fun call(context: Context?){
+        context!!.applicationContext.unregisterReceiver(mBatInfoReceiver)
+    }
 }
+
+class shakeBroadcastReceiver : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val sManager = context!!.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sensor = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sManager.registerListener(ShakeEventListener(), sensor, SensorManager.SENSOR_DELAY_NORMAL)
+
+    }
+
+}
+class ShakeEventListener : SensorEventListener {
+
+    val threshold = 1000
+    var last_y = 0f
+    var last_z = 0f
+    var last_x = 0f
+    var lastUpdate = 0L
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+            handleShake(event)
+    }
+    fun handleShake(event: SensorEvent){
+        val curTime = System.currentTimeMillis()
+
+        if (curTime - lastUpdate > 100) {
+            val diffTime = curTime - lastUpdate
+            lastUpdate = curTime
+            val x = event.values[SensorManager.DATA_X]
+            val y = event.values[SensorManager.DATA_Y]
+            val z = event.values[SensorManager.DATA_Z]
+
+            val speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000
+
+            if (speed > threshold) {
+               Log.d("hey","printed")
+            }
+            last_x = x
+            last_y = y
+            last_z = z
+        }
+        }
+ }
+
+
--- app/src/main/java/com/gft/robin/MainActivity.java
+++ app/src/main/java/com/gft/robin/MainActivity.java
@@ -1,62 +1,77 @@
 package com.gft.robin;
 
-import android.app.LauncherActivity;
-import android.app.Notification;
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.BatteryManager;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    Uri notification;
-    Ringtone r;
-    @Override
-    public void onCreate(Bundle b) {
-        super.onCreate(b);
-        setContentView(R.layout.activity_main);
-        Button cancel = (Button)findViewById(R.id.cancel);
-        cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-                r = RingtoneManager.getRingtone(getApplicationContext(), notification);
-                r.play();
-            }
-        });
-    }
-
+    Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+    MediaPlayer mMediaPlayer = new MediaPlayer();
     BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-            if(level == 100) {
-               try {
-                   notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-                   r = RingtoneManager.getRingtone(context.getApplicationContext(), notification);
-                   r.play();
-                   PackageManager pm = context.getPackageManager();
-                   Intent launchIntent = pm.getLaunchIntentForPackage("com.gft.robin");
-                   context.startActivity(launchIntent);
-                   context.getApplicationContext().unregisterReceiver(mBatInfoReceiver);
+            if (level == 98 || level == 99 || level == 100) {
+                try {
+                    mMediaPlayer.setDataSource(context, alert);
+                    mMediaPlayer.setLooping(true);
+                    mMediaPlayer.prepare();
+                    mMediaPlayer.start();
+                    PackageManager pm = context.getPackageManager();
+                    Intent launchIntent = pm.getLaunchIntentForPackage("com.gft.robin");
+                    context.startActivity(launchIntent);
+                    context.unregisterReceiver(mBatInfoReceiver);
                 } catch (Exception e ){
                     e.printStackTrace();
                 }
-
             }
         }
 
     };
+
+    @Override
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_main);
+        Button start = (Button) findViewById(R.id.start);
+        Button stop = (Button) findViewById(R.id.stop);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BatteryService.class);
+                startService(intent);
+                finish();
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(android.os.Build.VERSION.SDK_INT >= 21) {
+                    finishAndRemoveTask();
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                }
+                else {
+                    finish();
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                }
+            }
+        });
+    }
 
 }
